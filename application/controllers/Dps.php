9d0204c557f5c6039ff80e6eafac7ff8f59e1727
tarek-srit <tarek@springrainit.com>
2016-10-23 17:19:30 +0600

    Add Non Bank in car,Home,Personal loan

--- application/controllers/Dps.php
+++ application/controllers/Dps.php
@@ -79,7 +79,7 @@ class Dps extends CI_Controller
                 $result = $this->Common_model->update();
 
                 if ($result) {
-                    redirect(base_url().'dps/edit_tenure/success');
+                    redirect(base_url().'dps/tenure/success');
                 } else {
                     redirect(base_url().'dps/edit_tenure/error');
                 }
@@ -89,5 +89,87 @@ class Dps extends CI_Controller
         }
     }
 
+    public function monthly_installment($msg=''){
+        if ($this->session->userdata('email_address')) {
+            if ($msg == 'success') {
+                $data['feedback'] = '<div id="message" class="text-center alert alert-success">Successfully Save !!</div>';
+            } else if ($msg == 'error') {
+                $data['feedback'] = '<div id="message" class=" text-center alert alert-danger">Problem to Insert !!</div>';
+            }
+            $this->form_validation->set_rules('txtMonthlyInstallment', 'Monthly Installment', 'trim|required|is_unique[dps_monthly_installment.monthly_installment]');
+            if ($this->form_validation->run() == FALSE) {
+                $data['title'] = "Finager - Monthly Installment";
+                $this->load->view('admin/block/header', $data);
+                $this->load->view('admin/block/left_nav');
+                $this->load->view('admin/dps/monthly_installment');
+                $this->load->view('admin/block/footer');
+            }else{
+                $date = date('Y-m-d h:i:s');
+                $this->Common_model->data = array(
+                    'monthly_installment' => $this->input->post('txtMonthlyInstallment'),
+                    'created' => $date ,
+                    'created_by'=>$this->session->userdata('admin_user_id')
+                );
+                $this->Common_model->table_name = 'dps_monthly_installment';
+                $result = $this->Common_model->insert();
+//                print_r($this->data);
+
+//                echo $result; die;
+                if ($result) {
+                    redirect(base_url().'dps/monthly_installment/success');
+                } else {
+                    redirect(base_url().'dps/monthly_installment/error');
+                }
+            }
+        }else {
+            redirect(base_url().'backdoor');
+        }
+    }
+
+    public function edit_monthly_installment($msg=''){
+        if ($this->session->userdata('email_address')) {
+            if ($msg == 'success') {
+                $data['feedback'] = '<div id="message" class="text-center alert alert-success">Successfully Update !!</div>';
+            } else if ($msg == 'error') {
+                $data['feedback'] = '<div id="message" class=" text-center alert alert-danger">Problem to Update !!</div>';
+            }
+            $this->form_validation->set_rules('txtMonthlyInstallment', 'Monthly Installment', 'trim|required|numeric');
+
+
+            if ($this->form_validation->run() == FALSE) {
+                $data['title'] = "Finager - Edit Monthly Installment";
+                $this->load->view('admin/block/header', $data);
+                $this->load->view('admin/block/left_nav');
+                $this->load->view('admin/dps/edit_monthly_installment');
+                $this->load->view('admin/block/footer');
+            }else{
+                $this->Common_model->data = array(
+                    'monthly_installment' => htmlentities($this->input->post('txtMonthlyInstallment')),
+                    'modified_by'=>$this->session->userdata('admin_user_id')
+                );
+                $this->Common_model->table_name = 'dps_monthly_installment';
+                $this->Common_model->where = array('id' => $this->input->post('txtMonthlyInstallmentId'));
+                $result = $this->Common_model->update();
+
+                if ($result) {
+                    redirect(base_url().'dps/monthly_installment/success');
+                } else {
+                    redirect(base_url().'dps/edit_monthly_installment/error');
+                }
+            }
+        }else {
+            redirect(base_url().'backdoor');
+        }
+    }
+
+    function add_info($msg=''){
+
+
+        if ($result) {
+            redirect(base_url().'dps/monthly_installment/success');
+        } else {
+            redirect(base_url().'dps/edit_monthly_installment/error');
+        }
+    }
 
 }
--- application/views/admin/dps/edit_monthly_installment.php
+++ application/views/admin/dps/edit_monthly_installment.php
@@ -1,16 +1,16 @@
 <?php
 
-if(isset($_GET['tenure_id']))
+if(isset($_GET['installment_id']))
 {
-    $id=$_GET['tenure_id'];
-    $table='dps_tenure';
+    $id=$_GET['installment_id'];
+    $table='dps_monthly_installment';
     $id_field='id';
     $this->Delete_model->Delete_Single_Row($id,$table,$id_field);
 }
 
 if(isset($_GET['id'])){
     $id=$_GET['id'];
-    $table='dps_tenure';
+    $table='dps_monthly_installment';
     $id_field='id';
     $row=$this->Select_model->Select_Single_Row($id,$table,$id_field);
     /*print_r($row);
@@ -18,7 +18,7 @@ if(isset($_GET['id'])){
         die;*/
 }else{
     $row['id']='';
-    $row['tenure']='';
+    $row['monthly_installment']='';
 }
 ?>
 <!-- MAIN PANEL -->
@@ -35,7 +35,7 @@ if(isset($_GET['id'])){
 
         <!-- breadcrumb -->
         <ol class="breadcrumb">
-            <li>Deposit</li><li>DPS</li><li>Tenure </li>
+            <li>Deposit</li><li>DPS</li><li>Update Monthly Installment </li>
         </ol>
         <!-- end breadcrumb -->
     </div>
@@ -50,12 +50,12 @@ if(isset($_GET['id'])){
                     <i class="fa fa-table fa-fw "></i>
                   Deposit
 							<span>> 
-								Update Tenure
+								Update Monthly Installment
 							</span>
                 </h1>
             </div>
             <div class="col-xs-12 col-sm-5 col-md-5 col-lg-8">
-                <a href="<?php echo base_url();?>dps/tenure/" class="btn btn-primary pull-right">Add +</a>
+                <a href="<?php echo base_url();?>dps/monthly_installment/" class="btn btn-primary pull-right">Add +</a>
             </div>
 
         </div>
@@ -73,7 +73,7 @@ if(isset($_GET['id'])){
                     <div class="jarviswidget jarviswidget-color-darken" id="wid-id-1" data-widget-editbutton="false" data-widget-custombutton="false">
                         <header>
                             <span class="widget-icon"> <i class="fa fa-edit"></i> </span>
-                            <h2>Update Tenure</h2>
+                            <h2>Update Monthly Installment</h2>
 
                         </header>
 
@@ -90,7 +90,7 @@ if(isset($_GET['id'])){
                             <!-- widget content -->
                             <div class="widget-body no-padding">
 
-                                <form id="age_limit" method="post" action="<?php echo base_url();?>dps/edit_tenure" class="smart-form" novalidate="novalidate">
+                                <form id="age_limit" method="post" action="<?php echo base_url();?>dps/edit_monthly_installment" class="smart-form" novalidate="novalidate">
                                     <?php
                                     //-----Display Success or Error message---
                                     if(isset($feedback)){
@@ -99,14 +99,14 @@ if(isset($_GET['id'])){
                                     ?>
                                     <fieldset>
                                         <section>
-                                            <label class="label"> Tenure ( Write 0nly year number ex: 1, 3, 5 )</label>
+                                            <label class="label"> Monthly Installment</label>
                                             <label class="input">
-                                                <input type="hidden" name="txtTenureId" value ="<?php echo $row['id']; ?>" />
-                                                <input type="text" name="txtTenure" placeholder="Write Tenure" value ="<?php echo $row['tenure']; ?>" />
+                                                <input type="hidden" name="txtMonthlyInstallmentId" value ="<?php echo $row['id']; ?>" />
+                                                <input type="text" name="txtMonthlyInstallment" placeholder="Write Monthly Installment Ex: 200,500" value ="<?php echo $row['monthly_installment']; ?>" />
 
                                             </label>
                                             <div class="form-group">
-                                                <label class="red"><?php echo form_error('txtTenure');?></label>
+                                                <label class="red"><?php echo form_error('txtMonthlyInstallment');?></label>
 
                                             </div>
                                         </section>
@@ -132,7 +132,6 @@ if(isset($_GET['id'])){
                 </article>
                 <!-- END COL -->
 
-
                 <!-- NEW WIDGET START -->
                 <article class="col-xs-12 col-sm-12 col-md-12 col-lg-6">
 
@@ -140,7 +139,7 @@ if(isset($_GET['id'])){
                     <div class="jarviswidget jarviswidget-color-darken" id="wid-id-0" data-widget-editbutton="false">
                         <header>
                             <span class="widget-icon"> <i class="fa fa-table"></i> </span>
-                            <h2>DPS Tenure List </h2>
+                            <h2>DPS Monthly Installment List </h2>
 
                         </header>
 
@@ -161,8 +160,7 @@ if(isset($_GET['id'])){
                                     <thead>
                                     <tr>
                                         <th data-hide="phone">No.</th>
-                                        <th data-class="expand"><i class="text-muted hidden-md hidden-sm hidden-xs"></i> DPS Tenure</th>
-                                        <th data-class="expand"><i class="text-muted hidden-md hidden-sm hidden-xs"></i> No of Installment</th>
+                                        <th data-class="expand"><i class="text-muted hidden-md hidden-sm hidden-xs"></i> DPS Monthly Installment</th>
                                         <th data-hide="phone"><i class="fa fa-pencil-square-o text-muted hidden-md hidden-sm hidden-xs"></i> Action</th>
 
                                     </tr>
@@ -171,7 +169,7 @@ if(isset($_GET['id'])){
 
                                     <?php
                                     $this->Common_model->order_column = 'id';
-                                    $this->Common_model->table_name = 'dps_tenure';
+                                    $this->Common_model->table_name = 'dps_monthly_installment';
                                     $query=$this->Common_model->select_all();
                                     $sl=1;
                                     foreach ($query->result() as $row)
@@ -179,9 +177,8 @@ if(isset($_GET['id'])){
                                         ?>
                                         <tr>
                                             <td class="text-center"><?php echo $sl; ?></td>
-                                            <td ><?php echo $row->tenure; echo ($row->tenure == 1) ? ' Year' : ' Years';?></td>
-                                            <td ><?php echo $row->no_of_installment;?> </td>
-                                            <td><a href="<?php echo base_url(); ?>dps/edit_tenure?id=<?php echo $row->id;?>" class="edit"><i class="fa fa-pencil-square-o fa-lg"></i></a><a href="?tenure_id=<?php echo $row->id;?>" onclick="return confirm('Are you really want to delete this item')" class="delete"> <i class="fa fa-trash-o fa-lg"></i></a></td>
+                                            <td >Tk. <?php echo $row->monthly_installment;?>.00</td>
+                                            <td><a href="<?php echo base_url(); ?>dps/edit_monthly_installment?id=<?php echo $row->id;?>" class="edit"><i class="fa fa-pencil-square-o fa-lg"></i></a><a href="?installment_id=<?php echo $row->id;?>" onclick="return confirm('Are you really want to delete this item')" class="delete"> <i class="fa fa-trash-o fa-lg"></i></a></td>
                                         </tr>
                                         <?php
                                         $sl++;
@@ -201,7 +198,6 @@ if(isset($_GET['id'])){
 
                 </article>
                 <!-- WIDGET END -->
-
 
 
             </div>
