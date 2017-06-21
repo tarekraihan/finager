4c883ca359022070bc8f5963ba03adccde1c870d
tarek-srit <tarek@springrainit.com>
2017-06-21 15:35:26 +0600

    Access Control

--- application/views/front_end/personal_loan.php
+++ application/views/front_end/personal_loan.php
@@ -1,135 +1,78 @@
 <style type="text/css">
-
 	.savingsContainer .leftCont {
-
 		padding: 0 0 0 0;
-
 	}
 
 	.calcSection .dragBox .drag {
-
 	    width: 200px;
-
 	}
 
 	.calcSection .dragBox .next {
-
 		right: 103px;
-
 	}
 
 	.calcSection .dragBox .drag2 {
-
 		width: 201px;
-
 	}
 
 	.monthWrap .calcSection .dragBox {
-
 		width: 215px;
-
 	}
 
 	.calcSection .dragBox .slideImg {
-
 		width: 210px;
-
 	}
 
 	.monthWrap .calcSection .dragBox .slideImg {
-
 		width: 207px;
-
 	}
 
 	.calcSection .dragBox .drag span {
-
 		background: none;
-
 	}
 
 	.card_details_pronsCons {
-
 		margin-top:0px;
-
 		border-bottom:0px;
-
 	}
-
 	.tPaybleAmount{
-
 		font-size:9px;
-
 	}
 
 	.btnPmore {
-
 		background-color: #f3f3f3;
-
 		border: 1px solid #EAEAEC;
-
 		padding: 1px 8px;
-
 		margin-bottom: 5px;
-
 		border-radius: 6px;
-
 		font-size: 12px;
-
 		font-weight: bold;
 
 	}
-
 	.btnPmore:hover {
-
 		background-color: #2B99BB;
-
 		color: #fff;
-
 		border: 1px solid #2B99BB;
 
 	}
-
-
-
 </style>
 
-
-
 	<section id="personal_header">
-
-<!--		--><?php
-
-//            pr($this->session->userdata());
-
-//        ?>
-
 	</section>
-
 	<section id="card">
-
 		<div class="container">
-
 			<div class="row">
-
 		<!-- Left bar query content start -->
-
 				<div class="col-sm-3 col-xs-3">
-
 					<div class="home_loan_left_bar">
-
 						<!-- slider range sidebar start-->
-
 						<div class="card_query">
-
 							<p>I Want </p>
-
 							<div class="query_radio">
 
 								<div>
 
 									<?php
-
 									$this->Common_model->table_name = 'personal_loan_looking_for';
 									$this->Common_model->offset = 0;
 									$this->Common_model->limit = 3;
@@ -154,210 +97,96 @@
 
 								<div>
 
-									<!--a id="displayText" href="javascript:toggle();">More Filter <i class="fa fa-sort-desc fa-lg"></i></a-->
-
 									<button class="btnPmore btn-open" data-toggle="collapse" data-target="#demo">More</button>
 
 								</div>
 
-								<!--div id="toggleText" style="display: none"-->
-
-								<div id="demo" class="collapse <?php //echo ($this->session->userdata('personal_loan_i_want_id') > 3) ? 'in' : '' ?>">
-
+								<div id="demo" class="collapse
 									<?php
-
 									$this->Common_model->table_name = 'personal_loan_looking_for';
-
 									$this->Common_model->offset = 3;
-
 									$this->Common_model->limit = 20;
-
 									$result = $this->Common_model->select_all();
-
 									foreach($result->result() as $row){
-
 										?>
-
 										<label class="material_radio_group">
-
 											<input type="radio" name="i_want"  id="iWant<?php echo $row->id; ?>" value="<?php echo $row->id;?>" <?php //echo ($this->session->userdata('personal_loan_i_want_id')) ? 'checked' : '' ?> class="material_radiobox"/>
-
 											<span class="material_check_radio"></span>
-
 											<?php echo $row->personal_loan_looking_for;?>
-
 										</label><br/>
-
 									<?php } ?>
-
 								</div>
-
 							</div>
-
 						</div>
-
-						
-
-						
-
 						<div class="row">
-
 							<div class="span8">
-
 								<div class="singleColumn page common">
-
 									<div class="htmltextarea section">
-
-										<div class="innerMdlWrapper"> 
-
-										 	 <!-- Middle Inner START--> 
-
+										<div class="innerMdlWrapper">
+										 	 <!-- Middle Inner START-->
 										  	<div class="innerMdlInner">
-
 											    <div class="calcWrapper">
-
 											      	<div class="clear"></div>
-
 											      	<!--Calculator Banner END-->
-
 											      	<div class="selectCalcWrapper" style="display:none;">
-
 												        <div class="radiobox">
-
 												          <input type="radio" id="savingCalculator" value="" title="Year" name="selCalc">
-
 												          <label for="savingCalculator">Home Loan Calculator</label>
-
 												        </div>
-
 												        <div class="radiobox">
-
 												          <input type="radio" id="depositCalculator" value="" title="Year" name="selCalc"  checked="checked">
-
 												          <label for="depositCalculator">Personal Loan Calculator</label>
-
 												        </div>
-
 											      	</div>
-
-
-
 											      	<div class="savingsContainer emiContainer">
-
-											    
-
-												        <div class="leftCont"> 
-
-												              
-
+												        <div class="leftCont">
 												          <!--Amount Already Saved START-->
-
-
-
 													        <div class="slideWrapper" id="alreadySaved">
-
-													          
-
 													            <div class="inputWrapper"> <span class="rupee"></span>
-
 													              <div class="inputField">
-
 													                <div class="inpLft"></div>
-
 													                <div class="inpMdl">
-
 													                  <input type="text" name="" value="0" id="finalAssest" class="input_LoanAmt"/>
-
 													                </div>
-
 													                <div class="inpRt"></div>
-
 													              </div>
-
 													            </div>
-
 													            <div class="clear"></div>
-
-													            
-
 													            <!--Calculator Section START-->
-
 													            <div class="calcContainer">
-
 													              <div class="calcSection calborder">
-
 													                <div class="dragBox">
-
 													                  <div class="drag"> <span></span>
-
 													                    <div class="sliderHover"></div>
-
 													                    <div class="draggable ui-widget-content drag3"> </div>
-
 													                  </div>
-
 													                  <div class="clear"></div>
-
-													                  <!--<div class="highLight"><input type="text" value="0" id="dragAssest"/></div>-->
-
 													                  <div class="slideImg" id="finacialAssest">
-
 													                    <ul>
-
 													                    </ul>
-
 													                  </div>
-
 													                  <div class="prev"></div>
-
 													                  <div class="next active"></div>
-
 													                </div>
-
 													                <div class="hideVal">25000</div>
-
 													              </div>
-
 													            </div>
-
-													            <!--Calculator Section END--> 
-
-													            
-
+													            <!--Calculator Section END-->
 													        </div>
-
 													          <!--Amount Already Saved END-->
-
-													          
-
 													        <div class="slideWrapper hidden" id="avgSave">
-
-													           
-
 													            <div class="inputWrapper">
-
 													              <div class="inputField">
-
 													                <div class="inpLft"></div>
-
 													                <div class="inpMdl">
-
 													                  <input type="text" name="" value="1" id="finalCustAge" maxlength="3" class="input_LoanPeriod"/>
-
 													                </div>
-
 													                <div class="inpRt"></div>
-
 													              </div>
-
 													              <div class="inpRadio">
-
 													                <div id="tenureType">
-
 													                  <label>Year</label>
-
 													                </div>
-
 													              </div>
 
 													            </div>
