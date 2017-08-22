9f82032e447d10b529fd167ccfc68ab256948240
tarekraihan <tarekraihan@yahoo.com>
2017-08-22 21:09:41 +0600

    update home loan

--- application/views/front_end/home_loan.php
+++ application/views/front_end/home_loan.php
@@ -284,105 +284,51 @@
 
 <section id="card">
     <div class="container">
-
         <div class="row">
-
             <!-- Left bar query content start -->
-
             <div class="col-sm-3 col-xs-3">
-
                 <div class="home_loan_left_bar" id="sidebar">
-
                     <!-- slider range sidebar start-->
-
                     <div class="card_query">
-
                         <p>I Want </p>
-
                         <div class="query_radio">
-
                             <?php
-
                             $card_user = $this->Select_model->select_all('home_loan_looking_for','ASC');
-
                             foreach($card_user->result() as $row){
-
                                 ?>
-
                                 <label class="material_radio_group">
-
                                     <input type="radio" name="iWant" id="iWant<?php echo $row->id; ?>" value="<?php echo $row->id;?>" class="material_radiobox"  <?php echo ($this->session->userdata("home_i_want") == $row->id) ? 'checked' :'' ?>/>
-
                                     <span class="material_check_radio"></span>
-
                                     <?php echo $row->home_loan_looking_for;?>
-
                                 </label><br/>
-
                             <?php
-
                             }
-
                             ?>
-
                         </div>
-
                     </div>
-
                     <div class="row">
-
                         <div class="span8">
-
                             <div class="singleColumn page common">
-
                                 <div class="htmltextarea section">
-
                                     <div class="innerMdlWrapper">
-
                                         <!-- Middle Inner START-->
-
                                         <div class="innerMdlInner">
-
                                             <div class="calcWrapper">
-
                                                 <div class="clear"></div>
-
                                                 <!--Calculator Banner END-->
-
                                                 <div class="selectCalcWrapper" style="display:none;">
-
                                                     <div class="radiobox">
-
                                                         <input type="radio" id="savingCalculator" value="" title="Year" name="selCalc">
-
                                                         <label for="savingCalculator">Home Loan Calculator</label>
-
                                                     </div>
-
                                                     <div class="radiobox">
-
                                                         <input type="radio" id="depositCalculator" value="" title="Year" name="selCalc"  checked="checked">
-
                                                         <label for="depositCalculator">Personal Loan Calculator</label>
-
                                                     </div>
-
                                                 </div>
-
-
-
                                                 <div class="savingsContainer emiContainer">
-
-
-
                                                     <div class="leftCont">
-
-
-
                                                         <!--Amount Already Saved START-->
-
-
-
                                                         <div class="slideWrapper" id="alreadySaved">
                                                             <div class="inputWrapper"> <span class="rupee"></span>
                                                                 <div class="inputField">
@@ -402,21 +348,12 @@
                                                                             <div class="sliderHover"></div>
                                                                             <div class="draggable ui-widget-content drag3"> </div>
                                                                        </div>
-
                                                                         <div class="clear"></div>
-
                                                                         <!--<div class="highLight"><input type="text" value="0" id="dragAssest"/></div>-->
-
                                                                         <div class="slideImg" id="finacialAssest">
-
-                                                                            <ul>
-
-                                                                            </ul>
-
+                                                                            <ul></ul>
                                                                         </div>
-
                                                                         <div class="prev"></div>
-
                                                                         <div class="next active"></div>
                                                                     </div>
                                                                     <div class="hideVal">50000</div>
@@ -524,7 +461,6 @@
                         </div>
                     </div>
                     <!-- slider range sidebar start-->
-
                     <div class="card_query">
                         <p>I Am</p>
                         <div class="query_radio">
@@ -625,58 +561,14 @@
         }
 
     });
-$(document).ready(function(){
-    $('#filter-carousel').carousel({
-        interval: false
-    });
-});
 
-</script>
-<script>
-
-    /*$(document).on("scroll",function () {
-        var offsetToTop = parseInt($(this).scrollTop());
-        var stickySidebar = $('#sidebar').offset() || { "top": NaN }.top;
-
-        if (offsetToTop > $(".footer").offset().top-600) {
-            console.log("as");
-            $("#sidebar").removeClass("fixed");
-            $("#sidebar").addClass("sidebar-absolute");
-            $("#sidebar").css("top",-offsetToTop);
-        }
-
-        else if (offsetToTop > 383) {
-            console.log("as2");
-            $('#sidebar').addClass("fixed");
-            $("#sidebar").removeClass("sidebar-absolute");
-            $("#sidebar").removeAttr("style");
-        }
-
-        else if (offsetToTop < 383) {
-            $("#sidebar").removeClass("fixed");
-            $("#sidebar").addClass("sidebar-absolute");
-            $("#sidebar").css("top",-offsetToTop);
-        }
-    });*/
 
     $(document).ready(function(){
-
-        function overlay(s, l) {
-            $('.overlay').remove();
-            if( s )
-                $('body').append('<div class="overlay" style="width:100%;height:100%;position:fixed;display:block;background:#000;opacity:0.7;top:0;left:0;z-index:1000;"></div>');
-            if( l )
-                $('.overlay').html('<div style="position:absolute;top:'+(document.documentElement.clientHeight/2)+'px;left:'+(document.documentElement.clientWidth/2)+'px;"><img src="<?php echo base_url();?>resource/front_end/images/loader.gif" width="100"></div>');
-            }
-        $(document).on('click','#pagination a',function(e){
-            e.preventDefault();
-            var cur_page = $(this).attr('data-ci-pagination-page'); // I haved test with attr('href') but not ok.
-//            alert(cur_page);
-            loadData(cur_page);
+        $('#filter-carousel').carousel({
+            interval: false
         });
 
         function loadData( page = null ){
-//            loading_show();
 			 var amount = $('#finalAssest').val();
             var principal_amount = "&home_principal_amount="+amount;
             var month = $('#finalLiability').val();
@@ -713,11 +605,9 @@ $(document).ready(function(){
                 data: main_string,
                 cache: false,
                 beforeSend: function() {
-//                    loading_show();
                     overlay(true,true);
                 },
                 success: function(msg){
-//                    loading_hide();
                     overlay(false);
                     $("#searchHomeLoan").html(msg);
                 }
@@ -731,26 +621,22 @@ $(document).ready(function(){
 		// Stop dragging calculator and fire event for search
          $(".draggable").on("dragstop",function(ev,ui){
             setTimeout(function(){ //Updated by Tarek on 14-05-2017
-                //alert($("#finalAssest").val());
                 loadData(page = null);
             }, 1000);
         });
 
         $("#finalAssest,#finalLiability").change(function () {
-            //alert($("#finalAssest").val());
             loadData(page = null);
         });
 
         $(".leftCont").find(".next").click(function(){
             setTimeout(function(){ //Updated by Tarek on 14-05-2017
-                //alert($("#finalAssest").val());
                 loadData(page = null);
             }, 1000);
         });
 
         $(".leftCont").find(".prev").click(function(){
             setTimeout(function(){ //Updated by Tarek on 14-05-2017
-                //alert($("#finalAssest").val());
                 loadData(page = null);
             }, 1000);
         });
@@ -789,55 +675,6 @@ $(document).ready(function(){
             alert("Sorry");
         }else{
             if($(".cart_anchor").hasClass("img_active")){
-                // prev fly to element
-
-				/*
-
-                //Select item image and pass to the function
-
-                var itemImg = $(this).parents('.full-card').find('.selected_card').eq(0);
-
-                flyToElement($(itemImg), $('.cart_anchor01'));
-
-                $(".cart_anchor01").addClass("img_active");
-
-                $(this).addClass("hidden");
-
-
-
-               // var itemImg = $(this).parents('div:eq(0)').find('.selected_card').eq(0);
-
-                var  formData = $(this).data();
-
-                var home_id = "home_id="+formData.home_id;
-
-
-
-                setTimeout(function(){
-
-                    $.ajax
-
-                    ({
-
-                        type: "POST",
-
-                        url: "<?php echo base_url();?>home_loan/ajax_compare_home_loan_image",
-
-                        data: home_id,
-
-                        success: function(msg)
-
-                        {
-
-                            $(".cart_anchor01").html(msg);
-
-                        }
-
-                    });
-
-                });
-
-				*/
 
 				var cart01 = $('.cart_anchor01');
 				var imgtodrag01 = $(this).parents('.full-card').find('.selected_card').eq(0);
@@ -885,49 +722,6 @@ $(document).ready(function(){
                     });
                 });
             }else{
-
-				/*
-
-                //Select item image and pass to the function
-                var itemImg = $(this).parents('.full-card').find('.selected_card').eq(0);
-                flyToElement($(itemImg), $('.cart_anchor'));
-                $(".cart_anchor").addClass("img_active");
-                $(this).addClass("hidden");
-                var itemImg = $(this).parents('.full-card').find('.selected_card').eq(0);
-                var  formData = $(this).data();
-
-                var home_id = "home_id="+formData.home_id;
-
-                //alert(home_id);
-
-
-
-                setTimeout(function(){
-
-                    $.ajax
-
-                    ({
-
-                        type: "POST",
-
-                        url: "<?php echo base_url();?>home_loan/ajax_compare_home_loan_image",
-
-                        data: home_id,
-
-                        success: function(msg)
-
-                        {
-
-                            $(".cart_anchor").html(msg);
-
-                        }
-
-                    });
-
-                });
-
-				*/
-
 
 				var cart = $('.cart_anchor');
 				var imgtodrag = $(this).parents('.full-card').find('.selected_card').eq(0);
