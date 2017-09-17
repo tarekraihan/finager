cc615865ddd2be49f07f14f41387e9b0f266910d
shahriaz <shahriaz2012@gmail.com>
2017-09-17 19:10:32 +0600

    logo name

--- application/views/front_end/block/footer.php
+++ application/views/front_end/block/footer.php
@@ -5,7 +5,7 @@
 
                 <div class="row margin_bottom_15">
                     <div class="col-sm-6">
-                        <img class="pull-left footer-logo" src="<?php echo base_url();?>resource/front_end/images/FinAlfa.svg">
+                        <img title="Finager Logo" class="pull-left footer-logo" src="<?php echo base_url();?>resource/front_end/images/finager-logo.svg">
                     </div>
 
                     <div class="col-sm-6">
